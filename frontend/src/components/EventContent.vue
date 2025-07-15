f3af0f4bfbcca69ac2bd9bf60bf822537bb7d6cd
FrienD <friendjunk@gmail.com>
2025-07-15 19:10:56 +0700

    implement toggleForm and animation for popup form

--- frontend/src/components/EventContent.vue
+++ frontend/src/components/EventContent.vue
@@ -1,9 +1,11 @@
 <script setup>
 import EventCard from "./EventCard.vue";
+import EventForm from "./EventForm.vue";
 import { onMounted, ref } from "vue";
 import axios from "axios";
 
 const events = ref([]);
+const openForm = ref(false);
 
 const getEvents = async () => {
   try {
@@ -18,28 +20,88 @@ const getEvents = async () => {
     console.log(err);
   }
 };
+
+const toggleForm = () => {
+  openForm.value = !openForm.value;
+};
+
+const handleFormSubmit = () => {
+  toggleForm();
+  getEvents();
+};
+
 onMounted(() => {
   getEvents();
 });
 </script>
 
 <template>
-  <div class="container">
-    <EventCard
-      v-for="event in events"
-      :key="event.id"
-      imgUrl="https://cdn-s.cdprojektred.com/concert/gallery/9a9a114aa2a4cf8681ac5ec602c9d2e3_q90_1920x1280.jpeg"
-      :title="event.name"
-      :description="event.description"
-    />
+  <div>
+    <div class="button-container">
+      <button class="add-event" @click="toggleForm">+ Add event</button>
+    </div>
+    <transition name="form-popup">
+      <EventForm
+        :isOpen="openForm"
+        v-if="openForm"
+        @submit="handleFormSubmit"
+      />
+    </transition>
+    <div class="event-container">
+      <EventCard
+        v-for="event in events"
+        :key="event.id"
+        :title="event.title"
+        :description="event.description"
+        :date="event.event_date.split('T')[0]"
+        :imgUrl="event.image_url"
+        :location="event.location"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.container {
+.event-container {
   display: flex;
   justify-content: flex-start;
-  margin: 80px 100px;
+  margin: 10px 10px 80px 300px;
   flex-wrap: wrap;
+}
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+  margin: 30px 60px 0px 300px;
+}
+
+.add-event {
+  font-weight: bold;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid;
+  cursor: pointer;
+  background-color: #3d74b6;
+  color: white;
+  padding: 10px;
+}
+
+.add-event:hover {
+  background-color: #5a96d6;
+}
+
+.form-popup-enter-active,
+.form-popup-leave-active {
+  transition: opacity 0.3s ease, transform 0.3s ease;
+}
+.form-popup-enter-from,
+.form-popup-leave-to {
+  opacity: 0;
+  transform: translate(-50%, -60%) scale(0.95);
+}
+.form-popup-enter-to,
+.form-popup-leave-from {
+  opacity: 1;
+  transform: translate(-50%, -50%) scale(1);
 }
 </style>
